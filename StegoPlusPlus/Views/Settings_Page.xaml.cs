5f97fd3037759516f01dc0509a14b66a2310ffd6
Erikodiony <Erikodiony@live.com>
2017-08-16 18:14:39 +0700

    Initial Release

--- StegoPlusPlus/Views/Settings_Page.xaml.cs
+++ StegoPlusPlus/Views/Settings_Page.xaml.cs
@@ -1,20 +1,7 @@
-﻿using StegoPlusPlus.Controls;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Storage;
+﻿using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Media.Animation;
-using Windows.UI.Xaml.Navigation;
+using StegoPlusPlus.Controls;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
